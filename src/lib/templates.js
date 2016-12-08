2bf592ed9be23d7d2cf89f6739fe864465fc7c55
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2016-12-08 15:23:44 -0800

    add more examples

--- src/lib/templates.js
+++ src/lib/templates.js
@@ -13,6 +13,51 @@ const templates = [
     repoUrl: "https://github.com/rohanchandra/type-theme",
     previewImageUrl: "https://cloud.githubusercontent.com/assets/816965/5142407/19742e48-71d6-11e4-8d9d-fdfe010784f0.png",
   },
+  {
+    name: "Roots",
+    type: "site",
+    repoUrl: "https://github.com/netlify-templates/roots-base",
+  },
+  {
+    name: "Jekyll",
+    type: "cms",
+    repoUrl: "https://github.com/netlify-templates/jekyll-netlify-cms",
+  },
+  {
+    name: "Hexo",
+    type: "site",
+    repoUrl: "https://github.com/netlify-templates/hexo-base",
+  },
+  {
+    name: "Pelican",
+    type: "cms",
+    repoUrl: "https://github.com/netlify-templates/pelican-netlify-cms",
+  },
+  {
+    name: "Middleman",
+    type: "site",
+    repoUrl: "https://github.com/netlify-templates/middleman-base",
+  },
+  {
+    name: "Cactus",
+    type: "site",
+    repoUrl: "https://github.com/netlify-templates/cactus-base",
+  },
+  {
+    name: "Mkdocs",
+    type: "site",
+    repoUrl: "https://github.com/netlify-templates/mkdocs-base",
+  },
+  {
+    name: "Hugo",
+    type: "site",
+    repoUrl: "https://github.com/netlify-templates/hugo-base",
+  },
+  {
+    name: "Hexo",
+    type: "cms",
+    repoUrl: "https://github.com/netlify-templates/hexo-cms-example",
+  },
 ];
 
 export default templates;
