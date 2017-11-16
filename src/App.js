4299a6b24da0d2bfdecd57ddbf7a77ca418af018
Brian Douglas <bdougie@users.noreply.github.com>
2017-11-16 10:50:25 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,18 +10,8 @@ const App = () => {
       <div className="App-header">
         <h1>Template Picker</h1>
         <p>
-          Starting a site doesn't have to be hard, here are some examples to get you started.
+          Starting a site doesn't have to be hard, here are some examples to get you started. Add a new templates via <a href="https://github.com/bdougie/cms-templates">GitHub</a>
         </p>
-    {/*
-        <a href="https://github.com/bdougie/cms-templates">
-          <img
-            style={{ position: "absolute", top: 0, right: 0, border: 0 }}
-            src="https://camo.githubusercontent.com/52760788cde945287fbb584134c4cbc2bc36f904/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f77686974655f6666666666662e706e67"
-            alt="Fork me on GitHub"
-            data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png"
-          />
-        </a>
-    */}
       </div>
       <Templates />
     </div>
