2a77a0f7fbd9d4b89fe14e95dc64462e5a492abb
hexagoncircle <ryan.mulligan@netlify.com>
2022-12-20 11:27:07 -0800

    Move redirects into output root

--- .eleventy.js
+++ .eleventy.js
@@ -2,41 +2,49 @@ const fetch = require("node-fetch");
 const parseGitUrl = require("git-url-parse");
 
 module.exports = function (config) {
-
   // Add filters to Nunjucks
-  config.addFilter("squash", require("./filters/squash.js") );
-  config.addFilter("appendToName", require("./filters/fileNameAppend.js") );
-  config.addFilter("stringify", require("./filters/stringify.js") );
-  config.addFilter("capitalize", (text) => text.replace(/\b\w/g, (m) => m.toUpperCase()));
+  config.addFilter("squash", require("./filters/squash.js"));
+  config.addFilter("appendToName", require("./filters/fileNameAppend.js"));
+  config.addFilter("stringify", require("./filters/stringify.js"));
+  config.addFilter("capitalize", (text) =>
+    text.replace(/\b\w/g, (m) => m.toUpperCase())
+  );
 
   // Group posts and links into collections without leaning on tags
   config.addCollection("templates", async function (collection) {
     const templates = collection.getFilteredByGlob("src/site/template/*.md");
 
-    return await Promise.all(templates.map(async (template) => {
-      const { full_name } = parseGitUrl(template.data.repo);
+    return await Promise.all(
+      templates.map(async (template) => {
+        const { full_name } = parseGitUrl(template.data.repo);
 
-      template.data.stars = await fetch(`https://api.github.com/repos/${full_name}`, {
-        headers: {
-          Authorization: `token ${process.env.GITHUB_TOKEN}`
-        }
+        template.data.stars = await fetch(
+          `https://api.github.com/repos/${full_name}`,
+          {
+            headers: {
+              Authorization: `token ${process.env.GITHUB_TOKEN}`,
+            },
+          }
+        )
+          .then((res) => res.json())
+          .then((json) => json.stargazers_count);
+
+        return template;
       })
-        .then(res => res.json())
-        .then(json => json.stargazers_count);
-
-      return template;
-    }));
+    );
   });
+
+  // Add redirects file to root of output directory
+  config.addPassthroughCopy("_redirects");
 
   return {
     dir: {
       input: "src/site",
       output: "dist",
-      includes: "_includes"
+      includes: "_includes",
     },
-    templateFormats : ["njk", "md"],
-    htmlTemplateEngine : "njk",
-    markdownTemplateEngine : "njk"
+    templateFormats: ["njk", "md"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
   };
-
 };
